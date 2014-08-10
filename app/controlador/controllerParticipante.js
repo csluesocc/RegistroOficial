079edbc206737063f33d5004bcc2a5c04d7ff5ff
AGodinez <pistilloagodinez@gmail.com>
2014-08-10 13:57:38 -0600

    Separe el controller.js

--- app/controlador/controllerParticipante.js
+++ app/controlador/controllerParticipante.js
@@ -1,5 +1,4 @@
-var Participante = require('./modelo/participante'),
-	Evento = require('./modelo/evento');
+var Participante = require('../modelo/participante');
 
 // -------- Participantes -------- //
 
@@ -62,71 +61,6 @@ exports.removeParticipante = function(req, res) {
 				if (err)
 					res.send(err)
 				res.json(participante);
-			});
-		});
-}
-
-// -------- EVENTOS -------- //
-
-// Obtiene todos los objetos Evento de la base de datos
-exports.getEvento = function (req, res){
-	Evento.find(
-		function(err, evento) {
-			if (err)
-				res.send(err)
-					res.json(evento); // devuelve todos los las Eventos en JSON
-				}
-			);
-}
-
-// Guarda un objeto Evento en la base de datos
-exports.setEvento = function(req, res) {
-
-		// Creo el objeto Evento
-		Evento.create(
-			{nombreEvento : req.body.nombreEvento,ponente: req.body.ponente,lugar:req.body.lugar, tipo: req.body.tipo, fecha:req.body.fecha, hora:req.body.hora, estado:req.body.estado},
-			function(err, evento) {
-				if (err)
-					res.send(err);
-
-				// Obtine y devuelve todos los eventos tras crear uno de ellos
-				Evento.find(function(err, evento) {
-				 	if (err)
-				 		res.send(err)
-				 	res.json(evento);
-				});
-			});
-
-	}
-
-// Modificamos un objeto Evento de la base de datos
-exports.updateEvento = function(req, res){
-	Evento.update( {_id : req.params.evento_id},
-					{$set:{nombreEvento: req.body.nombreEvento,	ponente: req.body.ponente, lugar: req.body.lugar, tipo:req.body.tipo, fecha:req.body.fecha, hora:req.body.hora, estado:req.body.estado}},
-					function(err, evento) {
-						if (err)
-							res.send(err);
-
-				// Obtine y devuelve todos los eventos tras crear uno de ellos
-				Evento.find(function(err, evento) {
-				 	if (err)
-				 		res.send(err)
-				 	res.json(evento);
-				});
-			});
-	}
-
-// Elimino un objeto Evento de la base de Datos
-exports.removeEvento = function(req, res) {
-	Evento.remove({_id : req.params.evento_id}, function(err, evento) {
-		if (err)
-			res.send(err);
-
-			// Obtine y devuelve todos los eventos tras borrar uno de ellos
-			Evento.find(function(err, evento) {
-				if (err)
-					res.send(err)
-				res.json(evento);
 			});
 		});
 }
--- app/routes.js
+++ app/routes.js
@@ -1,28 +1,29 @@
 var Participante = require('./modelo/participante'),
 	Evento = require('./modelo/evento'),
-	Controller = require ('./controller');
+	ControllerParticipante = require ('./controlador/controllerParticipante'),
+	ControllerEvento = require ('./controlador/controllerEvento');
 
 module.exports = function(app) {
 
 	// -------- Participantes -------- //
 	// devolver todos las Participantes
-	app.get('/api/participante', Controller.getParticipante);
+	app.get('/api/participante', ControllerParticipante.getParticipante);
 	// Crear una nueva Participante
-	app.post('/api/participante', Controller.setParticipante);
+	app.post('/api/participante', ControllerParticipante.setParticipante);
 	// Modificar los datos de una Participante
-	app.put('/api/participante/:id_participante', Controller.updateParticipante);
+	app.put('/api/participante/:id_participante', ControllerParticipante.updateParticipante);
 	// Borrar una Participante
-	app.delete('/api/participante/:id_participante', Controller.removeParticipante);
+	app.delete('/api/participante/:id_participante', ControllerParticipante.removeParticipante);
 
 	// -------- EVENTOS -------- //
 	// devolver todos los eventos
-	app.get('/api/evento', Controller.getEvento);
+	app.get('/api/evento', ControllerEvento.getEvento);
 	// Crear un nuevo evento
-	app.post('/api/evento', Controller.setEvento);
+	app.post('/api/evento', ControllerEvento.setEvento);
 	// Modificar los datos de un evento
-	app.put('/api/evento/:evento_id', Controller.updateEvento);
+	app.put('/api/evento/:evento_id', ControllerEvento.updateEvento);
 	// Borrar un evento
-	app.delete('/api/evento/:evento_id', Controller.removeEvento);
+	app.delete('/api/evento/:evento_id', ControllerEvento.removeEvento);
 
 	// -------- APLICATION -------- //
 	app.get('*', function(req, res) {
